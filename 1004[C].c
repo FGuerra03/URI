afb80e4136e4497c99e0b569da63b8123ea45870
Felipe Guerra <fguerra0310@gmail.com>
2017-08-05 15:39:13 -0300

    Exercício 1004 [C]

---
+++ 1004[C].c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    int valor1, valor2, PROD;
+    scanf("%d %d", &valor1, &valor2);
+    PROD = valor1 * valor2;
+    printf("PROD = %d\n", PROD);
+    return 0;
+}
