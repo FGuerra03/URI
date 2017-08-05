38d5c2c6b1e739256158001f2ec4d83a69bb047f
Felipe Guerra <fguerra0310@gmail.com>
2017-08-05 15:35:58 -0300

    Exercício 1003[C]

---
+++ 1003[C].c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int valor1, valor2, soma;
+    scanf("%d %d", &valor1, &valor2);
+    soma = valor1 + valor2;
+    printf("SOMA = %d\n", soma);
+
+    return 0;
+}
