7475a2c2a01dcc67548a95ed8199eb58c23183ba
Felipe Guerra <fguerra0310@gmail.com>
2017-08-13 00:55:40 -0300

    1095[C]

---
+++ 1095[C].c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int i = 1, j = 60;
+    printf("I=1 J=60\n");
+
+    while(j > 0){
+        i += 3;
+        j -= 5;
+        printf("I=%d J=%d\n", i, j);
+    }
+
+    return 0;
+}
