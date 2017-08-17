9880a98ac298911a9e2e0819e2d03284f14d04b4
Felipe Guerra <fguerra0310@gmail.com>
2017-08-17 08:42:05 -0300

    Exercício 1174[C]

---
+++ 1174[C].c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    float v[100];
+
+    for(i = 0; i < 100; i++){
+        scanf("%f", &v[i]);
+        if(v[i] <= 10){
+            printf("A[%d] = %.1f\n", i, v[i]);
+        }
+    }
+
+    return 0;
+}
